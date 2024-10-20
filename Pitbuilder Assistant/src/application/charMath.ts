939dae0f1d289bab1dcd8d244f35eb242e95e667
Erikas <erikas@tps.lt>
2024-10-20 22:14:39 +0300

    Initial commit

---
+++ Pitbuilder Assistant/src/application/charMath.ts
@@ -0,0 +1,263 @@
+export class CharMath {}
+import classesJson from '../assets/Classes.json' assert { type: 'json' }
+import templatesJson from '../assets/Templates.json' assert { type: 'json' }
+function escapeRegExp(str: string) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
+}
+export function GetClassList() {
+  const enemies: EnemyClass[] = []
+  const stringy = JSON.stringify(classesJson)
+  const classesFromJson = JSON.parse(stringy) as EnemyClass[]
+  classesFromJson.forEach((element) => {
+    enemies.push(new EnemyClass(element))
+  })
+  const result: GroupedClass[] = []
+  enemies.forEach((item) => {
+    const arr = result.find((x) => x.Type == item.Type)
+    if (arr) result.find((x) => x.Type == item.Type)?.Values.push(item)
+    else {
+      const newGroup = new GroupedClass({ Type: item.Type, Values: [item] })
+      result.push(newGroup)
+    }
+  })
+
+  return result
+}
+
+export class GroupedClass {
+  Type: string = ''
+  Values: EnemyClass[] = []
+  public constructor(init?: Partial<GroupedClass>) {
+    Object.assign(this, init)
+  }
+}
+
+export function GetTemplateList() {
+  const result: EnemyTemplate[] = []
+  const stringy = JSON.stringify(templatesJson)
+  const templatesFromJson = JSON.parse(stringy) as EnemyTemplate[]
+  templatesFromJson.forEach((element) => {
+    result.push(new EnemyTemplate(element))
+  })
+  result.sort((a, b) => a.Name?.localeCompare(b.Name || '') || 0)
+  return result
+}
+function replacePlaceholders(
+  DmgMod: number,
+  BlockMod: number,
+  MoveMod: number,
+  PostAttack: string,
+  DefScaling: number,
+  OffScaling: number,
+  Tier: number,
+  Text: string,
+  Values: string[]
+) {
+  let result = Text
+  //if (!Text) return ''
+  Values?.forEach((av, i) => {
+    if (av.indexOf('PSTATK') < 0) {
+      const newVal = av
+        .replace(/DMG/g, DmgMod.toString())
+        .replace(/BLK/g, BlockMod.toString())
+        .replace(/MOV/g, MoveMod.toString())
+        .replace(/DEF/g, DefScaling.toString())
+        .replace(/OFF/g, OffScaling.toString())
+        .replace(/TIR/g, Tier.toString())
+
+      result = result.replace(new RegExp(escapeRegExp('{' + i + '}'), 'g'), eval(newVal).toString())
+    } else result = result.replace(new RegExp(escapeRegExp('{' + i + '}'), 'g'), PostAttack)
+  })
+  return result
+}
+export class Action {
+  Name: string | null = null
+  Text: string = ''
+  Values: string[] = []
+  public constructor(init?: Partial<Action>) {
+    Object.assign(this, init)
+  }
+}
+export class Passive {
+  Name: string | null = null
+  Text: string = ''
+  Values: string[] = []
+  public constructor(init?: Partial<Passive>) {
+    Object.assign(this, init)
+  }
+}
+export class PostAttack {
+  Name: string | null = null
+  Text: string = ''
+  Values: string[] = []
+  public constructor(init?: Partial<PostAttack>) {
+    Object.assign(this, init)
+  }
+}
+export class EnemyClass {
+  Name: string = ''
+  DefScaling: number | number = 1
+  OffScaling: number | number = 1
+  Actions: Action[] = []
+  Passives: Passive[] = []
+  Weaknesses: string[] = []
+  Resistances: string[] = []
+  PostAttack: PostAttack[] = []
+  Type: string = ''
+  HPBar: number = 0
+  //Id!: number
+  public constructor(init?: Partial<EnemyClass>) {
+    this.Resistances = []
+    this.Weaknesses = []
+    Object.assign(this, init)
+  }
+}
+export class EnemyTemplate {
+  Name: string | null = null
+  DefScaling: number = 0
+  OffScaling: number = 0
+  HpMod: number = 0
+  DmgMod: number = 0
+  MoveMod: number = 0
+  BlockMod: number = 0
+  Actions: Action[] = []
+  Passives: Passive[] = []
+  Weaknesses: string[] = []
+  Resistances: string[] = []
+  PostAttack: PostAttack[] = []
+  HPBar: number = 0
+  public constructor(init?: Partial<EnemyTemplate>) {
+    Object.assign(this, init)
+    console.log(this.HPBar)
+  }
+}
+export class Enemy {
+  Tier: number | number = 1
+  Name: string | null = null
+  Class: EnemyClass | EnemyClass = new EnemyClass()
+  Templates: EnemyTemplate[] = []
+
+  HPMod: number = 0
+  HP: number = 0
+  DmgMod: number = 0
+  MoveMod: number = 0
+  BlockMod: number = 0
+  Actions: Action[] = []
+  ActionsShown: { Name: string; Text: string }[] = []
+  PassivesShown: { Name: string; Text: string }[] = []
+  ResistancesShown: string = ''
+  WeaknessesShown: string = ''
+  Passives: Passive[] = []
+  PostAttack: string = ''
+  HPBar: number = 1
+  // changeClass(eclass: { value: EnemyClass }) {
+  //   //this.Class = eclass.value
+  //   this.update()
+  // }
+  // changeTemplates(templates: { value: EnemyTemplate[] }) {
+  //   this.Templates = templates.value
+  //   this.update()
+  // }
+  // changeTier(tier: { value: number }) {
+  //   this.Tier = tier.value
+  //   this.update()
+  // }
+  update() {
+    const DefScaling =
+      this.Class?.DefScaling +
+      this.Templates?.reduce((sum, current) => sum + current.DefScaling || 0, 0)
+    this.HPMod =
+      DefScaling * 7 + this.Templates?.reduce((sum, current) => sum + current.HpMod || 0, 0)
+    this.HP = (1 + this.Tier) * 5 + this.HPMod
+
+    const OffScaling =
+      this.Class?.OffScaling +
+      this.Templates?.reduce((sum, current) => sum + current.OffScaling || 0, 0)
+    this.DmgMod =
+      OffScaling * 2 +
+      this.Templates?.reduce((sum, current) => sum + current.DmgMod || 0, 0) +
+      (this.Tier - 1) * 2
+
+    this.HPBar =
+      1 + this.Class.HPBar + this.Templates?.reduce((sum, current) => sum + current.HPBar || 0, 0)
+    console.log(this.HPBar)
+    this.MoveMod = this.Templates?.reduce((sum, current) => sum + current.MoveMod || 0, 0)
+
+    this.BlockMod =
+      this.Templates?.reduce((sum, current) => sum + current.BlockMod || 0, 0) + (this.Tier - 1) * 2
+
+    this.PostAttack = ''
+    const PostAttacks = this.Class?.PostAttack.concat(
+      this.Templates?.reduce((sum, current) => sum.concat(current.PostAttack), [] as PostAttack[])
+    )
+    PostAttacks.forEach((p) => {
+      if (p.Text)
+        this.PostAttack +=
+          replacePlaceholders(
+            this.DmgMod,
+            this.BlockMod,
+            this.MoveMod,
+            this.PostAttack,
+            DefScaling,
+            OffScaling,
+            this.Tier,
+            p.Text,
+            p.Values
+          ) + ' '
+    })
+    const Resistances = this.Class?.Resistances.concat(
+      this.Templates?.reduce((sum, current) => sum.concat(current.Resistances), [] as string[])
+    )
+    const Weaknesses = this.Class?.Weaknesses.concat(
+      this.Templates?.reduce((sum, current) => sum.concat(current.Weaknesses), [] as string[])
+    )
+    const ResistancesFiltered = Resistances.filter((x) => !Weaknesses.find((y) => y == x)).filter(
+      (value, index, array) => array.indexOf(value) === index
+    )
+    const WeaknessesFiltered = Weaknesses.filter((x) => !Resistances.find((y) => y == x)).filter(
+      (value, index, array) => array.indexOf(value) === index
+    )
+    this.ResistancesShown = ResistancesFiltered.sort((a, b) => a.localeCompare(b)).join(', ')
+    this.WeaknessesShown = WeaknessesFiltered.sort((a, b) => a.localeCompare(b)).join(', ')
+    this.Actions = this.Class?.Actions.concat(
+      this.Templates?.reduce((sum, current) => sum.concat(current.Actions), [] as Action[])
+    )
+    this.ActionsShown = []
+    this.Actions.forEach((a) => {
+      this.ActionsShown.push({
+        Name: a.Name || '',
+        Text: replacePlaceholders(
+          this.DmgMod,
+          this.BlockMod,
+          this.MoveMod,
+          this.PostAttack,
+          DefScaling,
+          OffScaling,
+          this.Tier,
+          a.Text,
+          a.Values
+        )
+      })
+    })
+    this.Passives = this.Class?.Passives.concat(
+      this.Templates?.reduce((sum, current) => sum.concat(current.Passives), [] as Passive[])
+    )
+    this.PassivesShown = []
+    this.Passives.forEach((p) => {
+      this.PassivesShown.push({
+        Name: p.Name || '',
+        Text: replacePlaceholders(
+          this.DmgMod,
+          this.BlockMod,
+          this.MoveMod,
+          this.PostAttack,
+          DefScaling,
+          OffScaling,
+          this.Tier,
+          p.Text,
+          p.Values
+        )
+      })
+    })
+  }
+}
---
+++ Pitbuilder Assistant/src/components/Enemy.vue
@@ -0,0 +1,174 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { Enemy, GetClassList, GetTemplateList } from '@/application/charMath'
+import Multiselect from 'primevue/multiselect'
+import Select from 'primevue/select'
+import FloatLabel from 'primevue/floatlabel'
+import { onMounted, ref } from 'vue'
+const enemyClasses = GetClassList()
+const enemyTemplates = GetTemplateList()
+const currentEnemy = ref(new Enemy())
+const currHp = ref()
+const currBlock = ref()
+currBlock.value = 0
+const currBarrier = ref()
+currBarrier.value = 0
+const dealDmg = ref()
+const props = defineProps(['id'])
+const turnTakenId = 'turnTaken' + props.id
+function changed(ev: {}) {
+  currentEnemy.value.update()
+  currHp.value = currentEnemy.value.HP
+}
+function damage() {
+  let input = dealDmg.value
+  if (!input) return
+  if (currBlock.value > input) {
+    currBlock.value = currBlock.value - input
+    return
+  } else {
+    input = input - currBlock.value
+    currBlock.value = 0
+  }
+  if (currBarrier.value > input) {
+    currBarrier.value = currBarrier.value - input
+    return
+  } else {
+    input = input - currBarrier.value
+    currBarrier.value = 0
+  }
+  currHp.value = currHp.value - input
+  if (currentEnemy.value.HPBar > 1 && currHp.value <= 0) {
+    const checks = document.querySelectorAll('[id^="HpBar"]') as NodeListOf<HTMLInputElement>
+    let uncheckThis = -1
+    checks.forEach((x, index) => {
+      if (x.checked) uncheckThis = index
+    })
+    if (uncheckThis >= 0) checks[uncheckThis].checked = false
+    if (checks[0].checked) currHp.value = currentEnemy.value.HP + currHp.value
+  }
+}
+</script>
+<template>
+  <div class="row" style="display: inline-block">
+    <FloatLabel class="w-full md:w-56" variant="in" style="display: inline-block">
+      <Select
+        v-model="currentEnemy.Class"
+        filter
+        :options="enemyClasses"
+        option-group-label="Type"
+        option-group-children="Values"
+        option-label="Name"
+        class="w-full md:w-56"
+        @change="changed($event)"
+        inputId="classSelect"
+      ></Select>
+      <label for="classSelect">Class</label>
+    </FloatLabel>
+
+    <FloatLabel class="w-full md:w-56" variant="in" style="display: inline-block">
+      <Multiselect
+        v-model="currentEnemy.Templates"
+        filter
+        :options="enemyTemplates"
+        option-label="Name"
+        class="w-full md:w-80"
+        @change="changed($event)"
+        inputId="templateSelect"
+      />
+      <label for="templateSelect">Templates</label>
+    </FloatLabel>
+    <FloatLabel class="w-full md:w-56" variant="in" style="display: inline-block">
+      <Select
+        v-model="currentEnemy.Tier"
+        filter
+        :options="[1, 2, 3]"
+        class="w-full md:w-80"
+        @change="changed($event)"
+        inputId="tierSelect"
+      ></Select>
+      <label for="tierSelect">Tier</label>
+    </FloatLabel>
+  </div>
+  <div class="row">
+    <div v-if="currentEnemy.HPBar > 1" style="display: inline-block" class="row">
+      <div
+        v-for="cnt in currentEnemy.HPBar"
+        :key="cnt"
+        style="display: inline-block; width: 60px; font-size: 16px"
+      >
+        <input :id="'HpBar' + cnt" type="checkbox" checked />
+      </div>
+    </div>
+    <div id="HP" style="display: inline-block">HP: {{ currentEnemy.HP }}/</div>
+    <input
+      v-model="currHp"
+      type="number"
+      style="display: inline-block; width: 60px; font-size: 16px"
+      placeholder="HP"
+    />
+    <input
+      v-model="currBarrier"
+      type="number"
+      style="display: inline-block; width: 60px; font-size: 16px"
+      placeholder="BAR"
+    />
+    <input
+      v-model="currBlock"
+      type="number"
+      style="display: inline-block; width: 60px; font-size: 16px"
+      placeholder="BLK"
+    />
+    <div
+      v-if="currHp <= currentEnemy.HP / 2 && currHp > 0"
+      style="display: inline-block; color: crimson"
+    >
+      Bloodied
+    </div>
+    <div v-if="currHp <= 0" style="display: inline-block; color: crimson">Defeated</div>
+    <div class="row">
+      <input
+        v-model="dealDmg"
+        type="number"
+        style="display: inline-block; width: 60px; font-size: 16px"
+        placeholder="DMG"
+      />
+      <button style="display: inline-block" @click="damage()">Deal damage</button>
+    </div>
+    <div>
+      <div>Conditions</div>
+      <textarea type="text" style="display: inline-block; font-size: 16px"></textarea>
+    </div>
+    <div class="row">
+      <label :for="turnTakenId" style="display: inline-block">Turn taken?</label>
+      <input
+        :id="turnTakenId"
+        type="checkbox"
+        style="display: inline-block; width: 60px; font-size: 16px"
+      />
+    </div>
+  </div>
+  <div>
+    <h3>Actions</h3>
+    <div v-for="act in currentEnemy.ActionsShown" :key="act.Name">
+      <span style="font-weight: bold">{{ act.Name }}</span> - {{ act.Text }}
+    </div>
+    <div class="row">
+      <div id="Damage" style="display: inline-block">Basic damage: {{ currentEnemy.DmgMod }}</div>
+    </div>
+  </div>
+  <div v-if="currentEnemy.PassivesShown.length > 0">
+    <h3>Passives</h3>
+    <div v-for="pass in currentEnemy.PassivesShown" :key="pass.Name">
+      <span style="font-weight: bold">{{ pass.Name }}</span> - {{ pass.Text }}
+    </div>
+  </div>
+  <div v-if="currentEnemy.ResistancesShown.length > 0">
+    <span style="font-weight: bold">Resistances</span><br />
+    {{ currentEnemy.ResistancesShown }}
+  </div>
+  <div v-if="currentEnemy.WeaknessesShown.length > 0">
+    <span style="font-weight: bold">Weaknesses</span><br />
+    {{ currentEnemy.WeaknessesShown }}
+  </div>
+</template>
